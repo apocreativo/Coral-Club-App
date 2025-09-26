8fb10c4b80424648c6c2677a4fb12a751700fd57
apocreativo <apocreativo@gmail.com>
2025-09-26 18:57:57 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
-createRoot(document.getElementById("root")).render(<App />);
+import { DebugBoundary } from "./DebugBoundary.jsx";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <DebugBoundary>
+    <App />
+  </DebugBoundary>
+);
